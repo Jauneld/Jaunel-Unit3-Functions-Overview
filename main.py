0be83281ee99b1d05fff3f9602781bc2dde4583f
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 02:16:46 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#def say_hi(): #defining the function say_hi()
+  #print("Why hello there!") #when the function say_hi() is called, the computer would run this line of code which is printing "Why hello there!" on the console. 
+
+#def offer_drink():#defining the function offer_drink()
+  #print("Would you care for a spot of tea?") #when the function offer_drink() is called, the computer would run this line of code which is printing "Would you care for a spot of tea?" on the console.
+
+#def offer_food():#defining the function offer_food()
+  #print("Biscuit?")#when the function offer_food() is called, the computer would run this line of code which is printing "Biscuit?" on the console.
+
+#def say_bye():#defining the function say_bye()
+ # print("Cheerio then.")#when the function say_bye() is called, the computer would run this line of code which is printing "Cheerio then." on the console.
+
+#say_hi()
+#print("Welcome to the hospitality program!")
+#print("What's your name?")
+#offer_drink()
+#print("Oh, lovely")
+#offer_food()
+
+
+def multi():
+  n1=15
+  n2=30
+  return n1*n2
+  
+print(multi())
+
+def add(num1,num2):
+  result = num1 + num2
+  return result
+  
+  
+print(add(20,8))
